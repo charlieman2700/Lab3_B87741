140ca47993666a999bc37012bf115ee5a9eea9ae
charlieman2700 <carlos.ace27@gmail.com>
2022-05-03 14:24:44 -0600

    Se implementa la sección Song En SongModel.cs se implementa la estructura de datos de una canción En SongController.cs se crea una canción, "canción favorita" En Index.cshtml se crea la vista de la sección Song del menú de la página web

---
+++ Laboratorio3/Controllers/SongController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Laboratorio3.Controllers
+{
+    public class SongController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+
+            var favoriteSong = new Models.SongModel
+            {
+                Name = "Who am I",
+                Artist = "Casting Crowns",
+                Genre = "Worship",
+                ReleasedYear = 2003,
+                ImageUrl = "https://i.scdn.co/image/ab67616d0000b273b6b4afa5bec4ca0e9e586e2f"
+            };
+            ViewBag.MainTitle = "Favorite Song";
+
+            return View(favoriteSong);
+        }
+    }
+}
---
+++ Laboratorio3/Models/SongModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Laboratorio3.Models
+{
+    public class SongModel
+    {
+        
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public int ReleasedYear { get; set; }
+        public string ImageUrl { get; set; }
+
+    
+    }
+}
