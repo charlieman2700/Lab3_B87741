4badbabf6c725a329c0cf1c807195a0b1a0f7fa8
charlieman2700 <carlos.ace27@gmail.com>
2022-05-03 08:49:55 -0600

    Cascarón inicial del proyecto generado por visual studio

---
+++ Laboratorio3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Laboratorio3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
