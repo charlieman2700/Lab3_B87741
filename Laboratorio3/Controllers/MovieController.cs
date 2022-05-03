d0911cc3fe6276e03bd2ff82e42a3584095016d0
charlieman2700 <carlos.ace27@gmail.com>
2022-05-03 12:00:23 -0600

    Se modifican MovieController.cs, Index.cshtml _Layout.cshtml para incorporar una sección Movie en el menú el cual redirige hacia la vista Movie-Index donde se se despliegan películas creadas desde el controlador

--- Laboratorio3/Controllers/MovieController.cs
+++ Laboratorio3/Controllers/MovieController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Laboratorio3.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -13,7 +14,37 @@ namespace Laboratorio3.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            var movies = GetListOfMovies();
+            ViewBag.MainTitle = "List of my favorite Films";
+            return View(movies);
+        }
+
+        private List<MovieModel> GetListOfMovies()
+        {
+            List<MovieModel> movies = new List<MovieModel>();
+            movies.Add(new MovieModel
+            {
+                Id = 1,
+                Name = "Pulp Fiction",
+                Genre = "Crime/Drama",
+                ReleasedDate = new DateTime(1994, 10, 14)
+            });
+
+            movies.Add(new MovieModel
+            {
+                Id = 2,
+                Name = "Toy Story",
+                Genre = "Family/Comedy",
+                ReleasedDate = new DateTime(1995, 11, 22)
+            });
+            movies.Add(new MovieModel
+            {
+                Id = 3,
+                Name = "Mulan",
+                Genre = "Family/Comedy",
+                ReleasedDate = new DateTime(1998, 06, 19)
+            });
+            return movies;
         }
     }
 }
